ebb2a4520af9bb9c72195139c8c20b9ea0f76eb5
Taylor Huston <taylor.a.huston@gmail.com>
2017-03-02 20:45:46 -0800

    Finish basic plugin skeleton

---
+++ src/main/java/com/atlassian/tutorial/macro/helloworld.java
@@ -0,0 +1,18 @@
+package com.atlassian.tutorial.macro;
+
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
+
+import java.util.Map;
+
+public class helloworld implements Macro {
+
+    public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
+        return "<h1>Hello World</h1>";
+    }
+
+    public BodyType getBodyType() { return BodyType.NONE; }
+
+    public OutputType getOutputType() { return OutputType.BLOCK; }
+}
